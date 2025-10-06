7af28f2eebcb6c0a29cff201dcbd25e884c16680
ThisIsSantiX <thesantigeometrydash29@gmail.com>
2025-10-05 20:43:27 -0500

    a

--- config/app.php
+++ config/app.php
@@ -181,6 +181,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Hofmannsven\Brevo\BrevoServiceProvider::class,
+
 
         /*
          * Package Service Providers...
